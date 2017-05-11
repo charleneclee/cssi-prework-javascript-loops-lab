703f03405f2c3bae8eeec5890817f3ab3381ec16
charleneclee <cclee@google.com>
2017-05-10 19:24:06 -0700

    finished loops lab

--- beatles.js
+++ beatles.js
@@ -1,1 +1,30 @@
 'use strict';
+function johnLennonFacts(facts){
+  var excitingFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    excitingFacts.push(facts[i] + "!!!");
+    i = i + 1;
+  }
+  return excitingFacts
+}
+
+function theBeatlesPlay(musicians, instruments){
+  var beatlesRoles = [];
+  for(var i = 0; i < musicians.length; i = i +1){
+    beatlesRoles.push(musicians[i] + "plays" + instruments[i])
+  }
+  return beatlesRoles
+}
+
+function iLovetheBeatles(number){
+  var love = []
+  if(number <= 10 && number > 0);
+  var i = 0;
+  do {
+    love.push("I love the Beatles!");
+    i = i + 1;
+  } while (i < number){
+  return love
+}
+}
